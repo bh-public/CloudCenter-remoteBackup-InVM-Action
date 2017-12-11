485a757e203aaffca7409bdedbc6d06c38858758
bh-public <bh.public@comcast.net>
2017-12-11 10:24:30 -0800

    updates to script, comments

--- FTPremoteBackup.sh
+++ FTPremoteBackup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# This is a Cisco Cloud Center Action Lib Example
+# This is a Cisco Cloud Center Action Lib Example that uses FTP to move a file called 
+# DATA from /home/cliqruser to a remote ftp server with the user naming the new file.
+# 
 # Execute path is: 
 #   https://raw.githubusercontent.com/bh-public/remoteBackup/master/FTPremoteBackup.sh
 
@@ -12,7 +14,9 @@ source /usr/local/osmosix/service/utils/cfgutil.sh
 source /usr/local/osmosix/service/utils/agent_util.sh
 
 #actionSendMessage "Action - FTPremoteBackup started" 
-
+# We can un-comment the ftp install, but as part of the lab we will use
+# the Day 2 action called Yum install <pkgname> to install ftp.
+# 
 #sudo yum install ftp -y
 
 # Define our env vars
