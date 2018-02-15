88a4d7395510afa3135b1ee597aadfc7b4964fb1
bh-public <bh.public@comcast.net>
2018-02-15 14:11:14 -0800

    changed IP address to new server

--- FTPremoteBackup.sh
+++ FTPremoteBackup.sh
@@ -20,7 +20,8 @@ source /usr/local/osmosix/service/utils/agent_util.sh
 #sudo yum install ftp -y
 
 # Define our env vars
-HOST='13.57.9.139'
+#HOST='13.57.9.139'
+HOST='52.53.181.141'
 USER='selab'
 PASSWD='welcome2cliqr'
 #FILE='/home/cliqruser/DATA'
